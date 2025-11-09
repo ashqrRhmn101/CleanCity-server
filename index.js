50aca555ca1e05256f4b08fbbcdea4826a17dbf9
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-11-09 10:04:18 +0600

    Get all Issues & get latest Issues && get single issues by id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -26,11 +26,29 @@ async function run() {
   try {
     await client.connect();
 
-    const db = client.db("CleanCity")
-    const issuesCollection = db.collection('issues')
+    const db = client.db("CleanCity");
+    const issuesCollection = db.collection("issues");
 
+    // // Get => Find all Issues
+    app.get("/issues", async (req, res) => {
+      const result = await issuesCollection.find().toArray();
+      res.send(result);
+    });
 
+    // Latest Issues
+    app.get("/latest-issues", async (req, res) => {
+      const cursor = issuesCollection.find().sort({ date: -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    // Get => find ID single Issues
+    app.get("/issues/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await issuesCollection.findOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
